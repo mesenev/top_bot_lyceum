78d58baed6d83d3f77b1e17fb03d1836ad31e600
mesenev <mesenev.pr@gmail.com>
2017-10-11 15:11:12 +1000

    move config to config package

---
+++ config/__init__.py
@@ -0,0 +1,2 @@
+from .config_default import *
+from .config import *
---
+++ config/config.py
@@ -0,0 +1,12 @@
+# noinspection PyUnresolvedReferences
+from config.config_default import *
+
+
+BOT_TOKEN = '473815048:AAEWn19W9I5uPZMz3Bq_vDQuApDZyxIXfuY'
+LOGIN = ''
+PASSWORD = ''
+LOGIN_URL = 'https://lms.yandexlyceum.ru/accounts/login/'
+DATA_URL = 'https://lms.yandexlyceum.ru/course/34/gradebook/'
+CHAT_ID = '102660981'
+SESSION_ID = 'f312ef9712129db6fca7c95584774e88'
+
