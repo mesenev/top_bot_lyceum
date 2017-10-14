e05d5341d7de69916f3d6d27821dc8e3f3d5f332
Nikita Malyavin <nikitamalyavin@gmail.com>
2017-10-15 02:40:33 +1000

    storage: fix circular reference

--- infra/storage.py
+++ infra/storage.py
@@ -1,10 +1,9 @@
 import peewee
 
-import database
-
 db = peewee.SqliteDatabase('database.db')
 
 
 def setup_database():
+    import database
     db.connect()
     db.create_tables(database.models, safe=True)
