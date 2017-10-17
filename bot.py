27ac2ad9b17307a8a49c4b04d76a253c668da098
Nikita Malyavin <nikitamalyavin@gmail.com>
2017-10-18 03:28:23 +1000

    bot: remove empty or unused lines, optimize lines count

--- bot.py
+++ bot.py
@@ -11,12 +11,8 @@ updater = Updater(token=BOT_TOKEN)
 j = updater.job_queue
 
 infra.logging.setup_dispatcher_logging(updater.dispatcher)
-
-
 infra.storage.setup_database()
 infra.i18n.setup_locale()
-
-delta = datetime.time(hour=22, tzinfo=TIMEZONE)
 
 # TODO: Write flexible datetime
 j.run_daily(methods.get_top.send_msg,
@@ -25,11 +21,10 @@ j.run_daily(methods.get_top.send_msg,
 for handler in methods.METHODS:
     updater.dispatcher.add_handler(handler)
 
-updater.start_polling()
-
 if LOG_CHAT_ID:
     methods.start.send_startup_greeting(updater.bot, LOG_CHAT_ID)
 
+updater.start_polling()
 updater.idle()
 
 infra.storage.db.close()
