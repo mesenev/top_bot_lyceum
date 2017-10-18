a3099cea88b908443c17c72ce2a5ea95c3d01a6a
Nikita Malyavin <nikitamalyavin@gmail.com>
2017-10-18 21:11:27 +1000

    debug: add /raise handler

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,6 +1,7 @@
 from telegram.ext import CommandHandler, MessageHandler, Filters
 
 from . import auth
+from . import debug
 from . import get_top
 from . import homeworks
 from . import mojno
@@ -8,6 +9,7 @@ from . import start
 
 METHODS = [
     CommandHandler('start', start.on_start),
+    CommandHandler('raise', debug.on_raise),
     MessageHandler(Filters.text & Filters.group, mojno.send_msg),
     CommandHandler('top', get_top.send_msg),
     CommandHandler('top-activate', get_top.top_activate),
